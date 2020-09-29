bf477431686bb9b8bcbbe7c0d7edd7c0c04e8d22
Álvaro Prieto <alviprieto@hotmail.com>
2020-09-29 00:04:33 -0300

    Add value object PlayerPosition with exceptions and enum to domain

---
+++ src/Domain/Enums/PlayerPositionEnum.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Enums
+{
+    public enum PlayerPositionEnum
+    {
+        Goalkeeper = 1,
+        Defender = 2,
+        Midfielder = 3,
+        Forward = 4
+    }
+}
---
+++ src/Domain/Exceptions/PlayerPositionInvalidException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Domain.Exceptions
+{
+    public class PlayerPositionInvalidException : Exception
+    {
+        public PlayerPositionInvalidException(string positionName)
+            : base($"Position \"{positionName}\" is not a valid player position.")
+        {
+        }
+    }
+}
