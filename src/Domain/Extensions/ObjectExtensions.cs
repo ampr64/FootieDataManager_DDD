8633dace705b13def0bcee1fd3a76f5bc64eb8eb
Álvaro Prieto <alviprieto@hotmail.com>
2020-09-29 00:02:34 -0300

    Add Json and Object extension methods to domain.

---
+++ src/Domain/Extensions/ObjectExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Extensions
+{
+    public static class ObjectExtensions
+    {
+        public static bool IsSameTypeAs(this object obj, object other) =>
+            obj?.GetType().Equals(other?.GetType()) ?? other is null;
+    }
+}
